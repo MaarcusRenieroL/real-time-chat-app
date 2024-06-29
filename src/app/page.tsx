ab1ccd5704123dc007a5da99344b1a70718f6aa9
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-06-29 08:55:16 +0530

    init: init upstash redis database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "~/components/ui/button";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div>
       <h1>Real Time Chat App</h1>
