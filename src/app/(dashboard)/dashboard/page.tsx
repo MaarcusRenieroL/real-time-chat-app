4701eb43958737dc24dbd37dd1fe42a22e1586e7
MaarcusRenieroL <maarcusreniero.l@gmail.com>
2024-06-30 07:45:24 +0530

    feat: add base layout of chat app

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,15 @@
-import { auth } from "~/lib/auth";
-
-export default async function DashboardPage() {
-  const session = await auth();
+export default function DashboardPage() {
   return (
-    <div>
-      <h1>Chat App</h1>
-      {JSON.stringify(session)}
+    <div className="flex w-full justify-between items-center h-full">
+      <div className="rounded-md border-gray-200 p-4 border w-0.5/12 hidden lg:block h-full">
+        <h1>Chat App</h1>
+      </div>
+      <div className="rounded-md border-gray-200 p-4 border lg:w-4/12 w-full h-full">
+        <h1>Chat App</h1>
+      </div>
+      <div className="rounded-md border-gray-200 p-4 border w-7/12 hidden lg:block h-full">
+        <h1>Chat App</h1>
+      </div>
     </div>
   );
 }
